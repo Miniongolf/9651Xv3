1bad2f52b1fb21577c7a6faa2e358ba3c0c0164a
N Cheng <nicholas.cheng@ucc.on.ca>
2024-01-15 18:02:55 -0500

    Update constants.hpp
    Adjust drive curve and turning constant

--- include/subHeads/constants.hpp
+++ include/subHeads/constants.hpp
@@ -4,6 +4,6 @@
 constexpr float CATA_RATIO = (float)1/3;
 
 // CONSTANTS
-constexpr float TURN_CONST = 0.6;          // Turning speed multiplier
+constexpr float TURN_CONST = 0.75;          // Turning speed multiplier
 constexpr float DEFAULT_CONTROLLER_DEADZONE = 0.1; // Controller deadzone (cross)
-constexpr float STICK_CURVE_GAIN = 2;      // Joystick input curve gain
+constexpr float STICK_CURVE_GAIN = 1.7;      // Joystick input curve gain
