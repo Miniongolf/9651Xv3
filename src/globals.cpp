b2079a4bc1fe2b0af9154f7c0bd29be7b78fbdf0
N Cheng <nicholas.cheng@ucc.on.ca>
2024-02-02 18:32:59 -0500

    Update globals.cpp

--- src/globals.cpp
+++ src/globals.cpp
@@ -12,15 +12,15 @@ pros::IMU imu(12);
 
 // MOTOR GROUPS
 pros::MotorGroup normalLeftMotors({
-    pros::Motor(2, pros::E_MOTOR_GEAR_BLUE),
-    pros::Motor(6, pros::E_MOTOR_GEAR_BLUE),
-    pros::Motor(1, pros::E_MOTOR_GEAR_BLUE)
+    pros::Motor(-2, pros::E_MOTOR_GEAR_GREEN),
+    pros::Motor(-6, pros::E_MOTOR_GEAR_GREEN),
+    pros::Motor(1, pros::E_MOTOR_GEAR_GREEN)
 });
 
 pros::MotorGroup normalRightMotors({
-    pros::Motor(-12, pros::E_MOTOR_GEAR_BLUE),
-    pros::Motor(-19, pros::E_MOTOR_GEAR_BLUE),
-    pros::Motor(-11, pros::E_MOTOR_GEAR_BLUE)
+    pros::Motor(12, pros::E_MOTOR_GEAR_GREEN),
+    pros::Motor(19, pros::E_MOTOR_GEAR_GREEN),
+    pros::Motor(-11, pros::E_MOTOR_GEAR_GREEN)
 });
 
 pros::MotorGroup cataMotors({
